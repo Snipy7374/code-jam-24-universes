504070fa86140eca81a2dfda9ea54877ae5d0404
EarthKiii <jonas.charrier1@gmail.com>
2024-07-20 21:32:45 +0200

    Add Ad embed (part 2) (test)

--- src/ad.py
+++ src/ad.py
@@ -1,8 +1,10 @@
+import re
 from random import choices
 from string import ascii_letters
 
 import disnake
 import requests
+from html2text import HTML2Text
 
 type JSON = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
 
@@ -25,20 +27,28 @@ class Ad(disnake.Embed):
                 # ascii_letters, k=2 -> 26² = 676 possible ads at runtime
             },
             timeout=5,
-        ).json()[1]  # the second page_object is chosen to avoid the disambiguation pages
+        ).json()["pages"][1]  # the second page_object is chosen to avoid the disambiguation pages
 
         self.key: str = page_object["key"]
         title: str = page_object["title"]
         url: str = f"https://en.wikipedia.org/wiki/{self.key}"
-        image_url: str = page_object["thumbnail"]["url"]
+        image_url: str = f"https:{page_object["thumbnail"]["url"]}"
         html: str = requests.get(
             f"https://en.wikipedia.org/w/rest.php/v1/page/{self.key}/html",
             timeout=5,
-        ).text
+        ).text  # 4096 characters is the maximum length of the description
 
-        super().__init__(title=title, url=url, description=html)
+        html: str = re.sub(r"<figure.*?</figure>", "", html)
+        # HTML2Text doesn't strip figures even if ignore_images is True
 
-        self.set_image(url=image_url)
+        html2text = HTML2Text(baseurl=url, bodywidth=0)
+        html2text.ignore_images = True
+
+        description: str = html2text.handle(html)[:4096]
+
+        super().__init__(title=title, url=url, description=description)
+
+        self.set_thumbnail(url=image_url)
         self.set_footer(text="Unique Universes 2024")
 
     def __repr__(self) -> str:
