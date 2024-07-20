c1d39ce9d70d45f88ad180f8def5145216a95bd3
EarthKiii <jonas.charrier1@gmail.com>
2024-07-20 21:32:45 +0200

    Add Ad embed (part 2) (test)

--- src/exts/test/test_ad.py
+++ src/exts/test/test_ad.py
@@ -1,16 +1,17 @@
 import disnake
+from disnake.ext import commands
 from src.ad import Ad
 from src.bot import Universe
 
 
-class TestAd(disnake.Cog):
+class TestAd(commands.Cog):
     def __init__(self, bot: Universe) -> None:
         self.bot = bot
 
-    @disnake.slash_command(name="test ad", description="A command to test Ad")
+    @commands.slash_command(name="test_ad", description="A command to test Ad")
     async def test_ad(self, ctx: disnake.ApplicationCommandInteraction) -> None:
         await ctx.response.send_message("Test command executed.", embed=Ad())
 
 
-async def setup(bot: Universe) -> None:
+def setup(bot: Universe) -> None:
     bot.add_cog(TestAd(bot))
