182cfbc8e3b1644fac7d5ce9245f4b0644d303e0
stroh13 <strohmayeradam@gmail.com>
2024-07-23 16:18:54 +0200

    Add embed attributes to ShootMenu
    Added embed attributes, did ammunition check in shoot_callback

--- src/views/shoot.py
+++ src/views/shoot.py
@@ -9,6 +9,25 @@ class ShootMenu(disnake.ui.View):
     message: disnake.InteractionMessage
     angle_x: int = 0
     angle_y: int = 0
+    position: int = 0
+    angle: int = 0
+    ammunition: int = 3  # shots left
+    energy: int = 1  # moves left
+    speed: int = 0
+    velocity: int = 0
+    bullet_type: str = "?"
+    obstacles_in_range: int = 8
+    fluff_stat_x: int = 1
+    fluff_stat_y: int = 2
+    fluff_stat_z: int = 3
+    fluff_stat_a: int = 4
+    enemy_position: int = 10
+    enemy_health: int = 10
+    enemy_energy: int = 5
+    enemy_has_premium_skin: bool = False
+    enemy_has_vip_pass: bool = False
+    enemy_logins_in_a_row: int = 3
+    total_shots: int = 0
 
     def __init__(self, author: disnake.Member) -> None:
         super().__init__(timeout=None)
@@ -73,10 +92,18 @@ class ShootMenu(disnake.ui.View):
 
     @disnake.ui.button(style=disnake.ButtonStyle.danger, label="Shoot", row=1)
     async def shoot_callback(self, _: disnake.ui.Button[ShootMenu], inter: disnake.MessageInteraction) -> None:
-        await inter.send(
-            f"You are taking your shot at degree {math.atan2(self.angle_x, self.angle_y) * (180 / math.pi)}",
-            ephemeral=True,
-        )
+        if self.ammunition != 0:
+            self.ammunition -= 1
+            self.total_shots += 1
+            await inter.send(
+                f"You are taking your shot at degree {math.atan2(self.angle_x, self.angle_y) * (180 / math.pi)}",
+                ephemeral=True,
+                embed=self.message.embeds[0]
+                .set_field_at(2, name="Ammunition (Shots left)", value=self.ammunition - 1)
+                .set_footer(text=f"Total Shots: {self.total_shots}"),
+            )
+        else:
+            await inter.send("You can't shoot because you're out of ammunition.")
 
     @disnake.ui.button(style=disnake.ButtonStyle.green, emoji="➡️", row=1)
     async def aim_right_callback(self, _: disnake.ui.Button[ShootMenu], inter: disnake.MessageInteraction) -> None:
