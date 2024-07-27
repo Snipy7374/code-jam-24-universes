9976632f695b63ea1ccf01d0715995885f259b27
Astroyo <139684641+Astroyo@users.noreply.github.com>
2024-07-27 09:14:25 -0700

    new: database (#9)
    * added database
    
    * safety
    
    * Update src/database.py
    
    * Update src/database.py
    
    * Update src/database.py
    
    * Update src/database.py
    
    * lint and format
    
    * added type hinting for player obj
    
    * changed where db connection is made
    
    * implement custom run flow for db closure
    
    ---------
    
    Co-authored-by: Snipy7374 <100313469+Snipy7374@users.noreply.github.com>

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,18 @@
 from __future__ import annotations
 
 import logging
-from typing import override
+from typing import TYPE_CHECKING, override
 
 import disnake
 from disnake.ext import commands
 from src.constants import EnvVars
+from src.database import Database
 from src.logger import setup_logging
+
+if TYPE_CHECKING:
+    from asyncio import AbstractEventLoop
+
+    from aiosqlite import Connection
 
 __all__: tuple[str] = ("Universe",)
 
@@ -14,10 +20,12 @@ _log = logging.getLogger(__name__)
 
 
 class Universe(commands.InteractionBot):
-    def __init__(self) -> None:
+    def __init__(self, loop: AbstractEventLoop, db_connection: Connection) -> None:
         super().__init__(
             intents=disnake.Intents.none(),
+            loop=loop,
         )
+        self.database = Database(connection=db_connection)
 
     async def on_ready(self) -> None:
         _log.info(f"Logged in as {self.user}")
@@ -25,5 +33,7 @@ class Universe(commands.InteractionBot):
     @override
     async def start(self) -> None:  # type: ignore[reportincomplatibleMethodOverride]
         setup_logging()
+        _log.info("Loading extensions")
         self.load_extensions("./src/exts")
+        _log.info("Extensions loading finished")
         await super().start(EnvVars.BOT_TOKEN, reconnect=True)
