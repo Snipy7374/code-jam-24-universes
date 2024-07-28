fc0ef9bae12ad9bd2cf79d818822afd0b9090b03
Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
2024-07-28 17:42:56 +0200

    feat: implement an about command (#12)
    * Added about command with bot instantiat
    
    Instantiated the bot, and added bot.run() and implemented the /about command
    
    * Updated .gitignore
    
    Includes .history
    
    * Added about command with bot instantiat
    
    Instantiated the bot, and added bot.run() and implemented the /about command
    
    * Updated .gitignore
    
    Includes .history
    
    * fix about command
    
    * remove version field
    
    * Add Ad embed (part 1)
    
    * Add Ad embed (part 2) (test)
    
    * Use a json of ads instead of random Wikipedia articles
    
    * Revert "Merge branch 'main' into main"
    
    This reverts commit 47c243ce5daa5f5f34ac807232d05b91c41daee2, reversing
    changes made to e1a8c370005973d21b54840ee5f57a251f76a91c.
    
    * new: database (#9)
    
    * added database
    
    * safety
    
    * Update src/database.py
    
    * Update src/database.py
    
    * Update src/database.py
    
    * Update src/database.py
    
    * lint and format
    
    * added type hinting for player obj
    
    * changed where db connection is made
    
    * implement custom run flow for db closure
    
    ---------
    
    Co-authored-by: Snipy7374 <100313469+Snipy7374@users.noreply.github.com>
    
    * Added about command with bot instantiat
    
    Instantiated the bot, and added bot.run() and implemented the /about command
    
    * fix about command
    
    * add shoot command mention in about command response
    
    ---------
    
    Co-authored-by: stroh13 <strohmayeradam@gmail.com>
    Co-authored-by: EarthKiii <jonas.charrier1@gmail.com>
    Co-authored-by: Astroyo <139684641+Astroyo@users.noreply.github.com>

--- src/bot.py
+++ src/bot.py
@@ -4,7 +4,7 @@ import logging
 from typing import TYPE_CHECKING, override
 
 import disnake
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 from src.constants import EnvVars
 from src.database import Database
 from src.logger import setup_logging
@@ -19,6 +19,28 @@ __all__: tuple[str] = ("Universe",)
 _log = logging.getLogger(__name__)
 
 
+class FetchTasks:
+    def __init__(self, bot: Universe) -> None:
+        self.bot = bot
+
+    async def _fetch_shoot_cmd(self) -> None:
+        _log.info("Fetching shoot cmd")
+        cmds = await self.bot.fetch_global_commands()
+        for cmd in cmds:
+            if cmd.name != "shoot":
+                continue
+            if isinstance(cmd, disnake.APISlashCommand):
+                self.bot.shoot_cmd = cmd
+
+        if self.bot.shoot_cmd is not None:
+            _log.info("Shoot command fetched")
+
+    @tasks.loop(seconds=1, count=1)
+    async def fetch_cmd(self) -> None:
+        await self.bot.wait_until_ready()
+        await self._fetch_shoot_cmd()
+
+
 class Universe(commands.InteractionBot):
     def __init__(self, loop: AbstractEventLoop, db_connection: Connection) -> None:
         super().__init__(
@@ -26,9 +48,14 @@ class Universe(commands.InteractionBot):
             loop=loop,
         )
         self.database = Database(connection=db_connection)
+        self.shoot_cmd: disnake.APISlashCommand | None = None
+        self.task_cmd = FetchTasks(self)
 
     async def on_ready(self) -> None:
         _log.info(f"Logged in as {self.user}")
+
+        if not self.task_cmd.fetch_cmd.is_running():
+            self.task_cmd.fetch_cmd.start()
 
     @override
     async def start(self) -> None:  # type: ignore[reportincomplatibleMethodOverride]
